e9ed66f3ac4c4fe3560a87c66a4f58e02f3a3b44
teamzone <teamzone@aboutagile.com>
2014-11-11 05:11:13 +0000

    Refactored for adding more tests for completeness of Login and Register

---
+++ feature-test/steps/testVerifyNewUserEmailAddress.js
@@ -0,0 +1,85 @@
+/* jslint node: true */
+/* global before, afterEach, after, featureFile, scenarios, steps */
+"use strict";
+var path = require('path');
+var Yadda = require('yadda');
+var bcrypt = require('bcrypt');
+var assert = require('assert');
+var usermanagementservice = require('../../lib/UserManagementService'); // The library that you wish to test
+var databasefactory = require('../../lib/common/DatabaseFactory');
+var emailverifyservice = require('../../lib/EmailVerifyService');
+var token = require('token');
+
+Yadda.plugins.mocha.AsyncStepLevelPlugin.init();
+
+//creating a path that works for locations, Yaddas calls is not as good as node's require and you need
+//to be in the folder itself
+var featureFilePath = path.resolve(__dirname, '../features/VerifyNewUserEmailAddress.feature');
+var usersDb;
+var ums;
+var interpreter_context;
+var database;
+
+setupInterpreterContext();
+
+before(function(done) {
+    done();
+});
+
+after(function(done) {
+    // ensure a clean environment
+    // remove the user created going direct to DB rather than API
+    for (var i = 0; i < interpreter_context.createdUsers.length; i++) { 
+        var userCount = i;
+         usersDb.del(interpreter_context.createdUsers[i].email, { sync: true }, function(err) {
+             if (err) {
+                console.log('Error whilst deleting');
+                assert.ifError(err);
+             }
+             checkforcompletion(userCount, done);
+         });
+    }
+});
+
+function checkforcompletion(userCount, done)
+{
+    if (userCount === interpreter_context.createdUsers.length - 1) {
+     	if (database.redis)
+     		database.redis.quit();
+        database.leveldb.close();    
+        console.log('Completed cleanup');
+        done();
+    }
+}
+
+var library = require('./VerifyNewUserEmailAddress');
+var yadda = new Yadda.Yadda(library, { interpreter_context: interpreter_context });
+    
+featureFile(featureFilePath, function(feature) {
+
+    scenarios(feature.scenarios, function(scenario) {
+        var scenario_context = { loggedInUser: null };
+        steps(scenario.steps, function(step, done) {
+            yadda.yadda(step, { scenario_context: scenario_context }, done);
+        });
+        
+    });
+
+});
+
+function setupInterpreterContext()
+{
+    var dbf = new databasefactory();
+    var evs = new emailverifyservice();
+    
+    token.defaults.secret = 'ZZVV';
+    token.defaults.timeStep = 96 * 60 * 60; // 24h in seconds
+    
+    database = dbf.levelredis();
+    usersDb = dbf.userdb(database.leveldb);
+    
+    ums = new usermanagementservice(usersDb, bcrypt, token, evs);
+    
+    interpreter_context = { ums: ums, usersDb: usersDb, createdUsers: [], token: token};
+}
+
