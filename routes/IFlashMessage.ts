9055588a7ce07a9e1f5d2f09ad615dcbf539b662
teamzone <teamzone@aboutagile.com>
2015-01-08 05:48:15 +0000

    flash.js
    arduous checkin, missing files - be smaller next time

---
+++ routes/IFlashMessage.ts
@@ -0,0 +1,3 @@
+export interface IFlashMessage {
+  msg : string;  
+}
---
+++ routes/flash.ts
@@ -0,0 +1,8 @@
+import iflashMessage = require('./IFlashMessage');
+
+class Flash {
+  type : string;
+  messages : iflashMessage.IFlashMessage[];
+}
+
+export = Flash;
