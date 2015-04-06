a35037bd9a76dc70826b7205aa38a0edaf05b403
teamzone <teamzone@aboutagile.com>
2015-04-06 05:24:30 +0000

    exclude testAddplayer.js until it iss refactored

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function(grunt) {
           timeout: 10000
           //require: 'coverage/blanket'
         },
-        src: ['lib/test/*.js', 'lib/ts/test/*.js', 'routes/test/*.js', 'feature-test/steps/common/test/*.js', 'feature-test/steps/test*.js']
+        src: ['lib/test/*.js', 'lib/ts/test/*.js', 'routes/test/*.js', 'feature-test/steps/common/test/*.js', 'feature-test/steps/test*.js', '!feature-test/steps/testAddPlayer.js']
       },
       uitbuild: {
         options: {
